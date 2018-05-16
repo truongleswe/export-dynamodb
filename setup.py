04239e2ab56e5718338e01307f1825534d85d300
Truong Le <truong.le@move.com>
2018-05-15 18:36:25 -0700

    added circleci configuration

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 
-#from setuptools import setup, find_packages
-
 setup(
   name = 'export-dynamodb',
   packages = ['exportdynamodb', 'exportdynamodb.entrypoints'],
