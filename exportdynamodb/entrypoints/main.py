9046fd6344710894ea2f0e55ffb8859c232ed48e
Truong Le <travistrle@gmail.com>
2018-05-18 21:13:43 -0700

    feature/base (#2)

--- exportdynamodb/entrypoints/main.py
+++ exportdynamodb/entrypoints/main.py
@@ -7,5 +7,5 @@ import click
 @click.command()
 @click.option('--table', '-t', help='table name.')
 def main(table):
-  """Export DynamoDb Table."""
-  print('export dynamodb: {}'.format(table))
+    """Export DynamoDb Table."""
+    print('export dynamodb: {}'.format(table))
